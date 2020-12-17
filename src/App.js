a8c844d2d74468fa4504065507e1b3a7f2df417c
arman sepahvand <44382579+armansepahvand@users.noreply.github.com>
2020-12-17 11:16:32 -0700

    cleand the project to start from scratch

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
 
 import "./App.css";
 import React, { useEffect, useState } from "react";
-import { useForm } from "./hooks/useForm";
-import { Hello } from "./Hello";
+
 
 function App() {
   
-  const [values,handleChange] = useForm({email:'', password:''});
-  const [showHello, setShowHello] = useState(true);
+  
 
-  // useEffect(()=>{
-  //   console.log('render')
-
-  //   return ()=>{
-  //     console.log('unmount')
-  //   }
-  // },[]);
+  
 
   return (
-    <div>
-      <button onClick={()=> setShowHello(!showHello)}> toggle</button>
-      {showHello && <Hello/>}
-      <input
-        name="email"
-        value={values.email}
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        value={values.password}
-        onChange={handleChange}
-      />
-    </div>
+    
   );
 }
 
